9cf8b945ad726251d6b9ba2d8cc4601e7bee0a29
Леонид Мурашов <lenja-d@yandex.ru>
2021-10-03 19:03:14 +0300

    Удалил лишние пространства имен

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Threading.Tasks;
 using System.Linq;
-using System.Threading;
-using System.Threading.Channels;
 
 
 namespace ConsoleGeneralTrain
